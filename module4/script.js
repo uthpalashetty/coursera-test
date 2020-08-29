85ba00c1155265fa2a4a763f63ffe4bc681bf827
uthpalashetty <uthpalajshetty@gmail.com>
2020-08-29 23:10:14 +0530

    aa

---
+++ module4/script.js
@@ -0,0 +1,15 @@
+
+(function (window) {
+
+var names = ["john","joe","yakoov","uthpala","shetty","ram","jay"];
+for (var i in names) {
+
+  var firstLetter = ((names[i]).charAt(0)).toLowerCase();
+
+  if (firstLetter === 'j') {
+     window.byeSpeaker.speak(names[i]);
+  } else {
+     window.helloSpeaker.speak(names[i]);
+  }
+}
+})(window);
